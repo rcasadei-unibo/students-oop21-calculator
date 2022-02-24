86444185401fbe217f6f5f76cc219e897ae7b627
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-02-24 17:51:53 +0100

    new project

---
+++ README.md
@@ -0,0 +1,8 @@
+# OOP-elaborato-Calculator
+
+Project by : 
+-Agosta Alessandro
+-Alni Riccardo
+-Lepore Roberto
+-Pesic Marco
+-Riccardo Tassinari
---
+++ src/test/InitialTest.java
@@ -0,0 +1,30 @@
+package test;
+
+import static org.junit.Assert.assertTrue;
+/**
+ * this class test.
+ */
+public class InitialTest {
+	private final boolean test = true;
+    /**
+     * test javadoc.
+     */
+    @org.junit.Test
+    public void initialize() {
+        assertTrue(this.test);
+    }
+    /**
+     * test javadoc.
+     */
+    @org.junit.Test
+    public void operation() {
+        assertTrue(this.test);
+    }
+    /**
+     * test javadoc.
+     */
+    @org.junit.Test
+    public void closure() {
+        assertTrue(this.test);
+    }
+}
