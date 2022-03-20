b4d1137b03fb8eb2b2ace9ebc9c06c1d929515f3
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-03-20 14:24:17 +0100

    added tests for shiftR and shiftL operations

--- src/test/BitwiseOperationsTest.java
+++ src/test/BitwiseOperationsTest.java
@@ -1,5 +1,6 @@
 package test;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
 import utils.ConversionAlgorithms;
 /**
@@ -9,19 +10,6 @@ import utils.ConversionAlgorithms;
  */
 public class BitwiseOperationsTest {
     private static double TOLERANCE = 0.01;
-    private double not(final double n1, final double base) {
-        final var bits = ConversionAlgorithms.conversionToStringBinary((int) n1).toCharArray();
-        String toConvert = String.valueOf(bits[0]);
-        for (int i = 1; i < bits.length; i++) {
-            if (String.valueOf(bits[i]).equals("1")) {
-                toConvert = toConvert.concat("0");
-            }
-            else {
-                toConvert = toConvert.concat("1");
-            }
-        }
-        return ConversionAlgorithms.conversionToDecimal((int) base, toConvert);
-    }
     private double and(final double n1, final double n2) {
         return (int) n1 & (int) n2;
     }
@@ -37,17 +25,52 @@ public class BitwiseOperationsTest {
     private double shiftL(final double n1, final double n2) {
         return (int) n1 << (int) n2;
     }
+    private double not(final double n1, final double base) {
+        var stringBits = ConversionAlgorithms.conversionToStringBinary((int) n1);
+        stringBits=addLeadingZerosToByte(stringBits);
+        
+        final var bits = stringBits.toCharArray();
+        String toConvert = String.valueOf(bits[0]);
+        for (int i = 1; i < bits.length; i++) {
+            if (String.valueOf(bits[i]).equals("1")) {
+                toConvert = toConvert.concat("0");
+            }
+            else {
+                toConvert = toConvert.concat("1");
+            }
+        }
+        return ConversionAlgorithms.conversionToDecimal((int) base, toConvert);
+    }
+    //TODO ror, rol, nand, nor
+
+ private String addLeadingZerosToByte(String stringBits) {
+     //1100 = -4 0100 = 4        1.0000.0100 = -4 0.0000.0100 = 4
+     final var sign = stringBits.charAt(0);
+     stringBits = stringBits.substring(1);
+     while(stringBits.length()%8!=0) {
+         stringBits = "0".concat(stringBits);
+     }
+     return String.valueOf(sign).concat(stringBits);
+ }
+    /**
+     * private test for leadingZeros
+     */
+    @org.junit.Test
+    public void testByte() {
+        assertTrue("000000100".equals(this.addLeadingZerosToByte(ConversionAlgorithms.conversionToStringBinary(4))));
+        assertTrue("100000100".equals(this.addLeadingZerosToByte(ConversionAlgorithms.conversionToStringBinary(-4))));
+        
+    }
     /**
      * Test for "not" operation.
      */
     @org.junit.Test
     public void testNot() {
-        //100000 32     011111 31
+        
         //TODO add test
-        //assertEquals(-33.0, not(32.0), TOLERANCE);
-        assertEquals(5, not(10,2),TOLERANCE);
-        assertEquals(31,not(32,2),TOLERANCE);
-        //System.out.println(Integer.MAX_VALUE);
+        
+        assertEquals("011111011",ConversionAlgorithms.conversionToStringBinary((int)not(4.0,2)));
+        
     }
     /**
      * Test for "and" operation.
@@ -82,8 +105,26 @@ public class BitwiseOperationsTest {
         assertEquals(0.0, xor(32.0, 32.0), TOLERANCE);
         //(100001 33 xor 100000 32) = 000001 1
         assertEquals(1.0, xor(33.0, 32.0), TOLERANCE);
-        //(11111 31 xor 00000 0) = 11111 31
-        System.out.println(not(31,2));
-        assertEquals(31.0, xor(31.0, not(31,2)), TOLERANCE); 
+        //(11111 31 xor 11100000 0) = 11111111 255
+        //System.out.println(not(31,2));
+        assertEquals(255.0, xor(31.0, not(31,2)), TOLERANCE); 
+    }
+    @org.junit.Test
+    public void testShiftL() {
+        assertEquals(25*2,this.shiftL(25, 1),TOLERANCE);
+        assertEquals(25*4,this.shiftL(25, 2),TOLERANCE);
+        assertEquals(25*32,this.shiftL(25, 5),TOLERANCE);
+    }
+    @org.junit.Test
+    public void testShiftR() {
+        //11001 = 25      1100 = 8
+        assertEquals((int)25/2,this.shiftR(25, 1),TOLERANCE);
+        assertEquals((int)25/4,this.shiftR(25, 2),TOLERANCE);
+        //11001         11
+        assertEquals((int)25/8,this.shiftR(25, 3),TOLERANCE);
+        //1
+        assertEquals((int)25/16,this.shiftR(25, 4),TOLERANCE);
+        //0
+        assertEquals((int)25/32,this.shiftR(25, 5),TOLERANCE);
     }
 }
