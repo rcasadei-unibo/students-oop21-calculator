bcde2193aef3fd00d822914117d9eac7ea252e7e
Riccardo Tassinari <riccardo.tassinari9@studio.unibo.it>
2022-03-23 14:50:32 +0100

    implementation of the StandardCalculatorModel constructor to match the necessities of ScientificCalculatorModel

--- src/model/calculators/StandardCalculatorModel.java
+++ src/model/calculators/StandardCalculatorModel.java
@@ -18,41 +18,42 @@ public class StandardCalculatorModel extends CalculatorModel {
      *
      */
     public StandardCalculatorModel() {
-        //TODO change operation names'
-        this.unaryOpMap.putAll(Map.of("inverse", new CCUnaryOperator((n) -> this.inverse(n), 0, null),
-                                      "root", new CCUnaryOperator((n) -> this.root(n), 0, null),
-                                      "square", new CCUnaryOperator((n) -> this.square(n), 0, null)
-                ));
-        //TODO add precedence and Type
-        this.binaryOpMap.putAll(Map.of("sum", new CCBinaryOperator((n1, n2) -> this.sum(n1, n2), 0, null),
-                                       "sub", new CCBinaryOperator((n1, n2) -> this.sub(n1, n2), 0, null),
-                                       "mult", new CCBinaryOperator((n1, n2) -> this.mult(n1, n2), 0, null),
-                                       "div", new CCBinaryOperator((n1, n2) -> this.div(n1, n2), 0, null),
-                                       "modulo", new CCBinaryOperator((n1, n2) -> this.modulo(n1, n2), 0, null)
-                                       ));
+       //TODO change operation names'
+       //TODO add precedence and Type
+       super(Map.of("sum", new CCBinaryOperator((n1, n2) -> sum(n1, n2), 0, null),
+                    "sub", new CCBinaryOperator((n1, n2) -> sub(n1, n2), 0, null),
+                    "mult", new CCBinaryOperator((n1, n2) -> mult(n1, n2), 0, null),
+                    "div", new CCBinaryOperator((n1, n2) -> div(n1, n2), 0, null),
+                    "modulo", new CCBinaryOperator((n1, n2) -> modulo(n1, n2), 0, null)),
+                     Map.of(
+                    "inverse", new CCUnaryOperator((n) -> inverse(n), 0, null),
+                    "root", new CCUnaryOperator((n) -> root(n), 0, null),
+                    "square", new CCUnaryOperator((n) -> square(n), 0, null)));
+
     }
-    private double sum(final double n1, final double n2) {
+
+    private static double sum(final double n1, final double n2) {
         return n1 + n2;
     }
-    private double sub(final double n1, final double n2) {
+    private static double sub(final double n1, final double n2) {
         return n1 - n2;
     }
-    private double mult(final double n1, final double n2) {
+    private static double mult(final double n1, final double n2) {
         return n1 * n2;
     }
-    private double div(final double n1, final double n2) {
+    private static double div(final double n1, final double n2) {
         return n1 / n2;
     }
-    private double modulo(final double n1, final double n2) {
+    private static double modulo(final double n1, final double n2) {
         return n1 % n2;
     }
-    private double root(final double n1) {
+    private static double root(final double n1) {
         return Math.sqrt(n1);
     }
-    private double square(final double n1) {
+    private static double square(final double n1) {
         return (n1) * (n1);
     }
-    private double inverse(final double n1) {
+    private static double inverse(final double n1) {
         return 1 / n1;
     }
 }
