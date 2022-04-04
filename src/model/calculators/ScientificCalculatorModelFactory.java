cd025109aaeb40db4dacb00b37e95d412deb6484
Riccardo Tassinari <riccardo.tassinari9@studio.unibo.it>
2022-04-04 15:17:36 +0200

    added precedence values to the operations in the ScientificCalculatorModel

--- src/model/calculators/ScientificCalculatorModelFactory.java
+++ src/model/calculators/ScientificCalculatorModelFactory.java
@@ -5,6 +5,7 @@ import java.util.stream.LongStream;
 
 import utils.CCBinaryOperator;
 import utils.CCUnaryOperator;
+import utils.Type;
 
 /**
  * 
@@ -24,18 +25,18 @@ public final class ScientificCalculatorModelFactory {
      * @return CalculatorModelTemplate
      */
     public static CalculatorModel create() {
-        final Map<String, CCBinaryOperator> binaryOpMap = Map.of("log", new CCBinaryOperator((n, b) -> Math.log(n) / Math.log(b), 0, null),
-                     "nthRoot", new CCBinaryOperator((n, i) -> Math.pow(n, 1 / i), 0, null),
-                     "potenza", new CCBinaryOperator((n, e) -> Math.pow(n, e), 0, null));
-        final Map<String, CCUnaryOperator> unaryOpMap = Map.of("ln", new CCUnaryOperator((n) -> Math.log(n), 0, null),
-                     "abs", new CCUnaryOperator((n) -> Math.abs(n), 0, null),
-                     "factorial", new CCUnaryOperator((n) -> (double) LongStream.rangeClosed(1, Math.round(n)).reduce(1, (long n1, long n2) -> n1 * n2), 0, null),
-                     "sin", new CCUnaryOperator((n) -> Math.sin(n), 0, null),
-                     "cos", new CCUnaryOperator((n) -> Math.cos(n), 0, null),
-                     "tan", new CCUnaryOperator((n) -> Math.tan(n), 0, null),
-                     "csc", new CCUnaryOperator((n) -> 1 / Math.sin(n), 0, null),
-                     "sec", new CCUnaryOperator((n) -> 1 / Math.cos(n), 0, null),
-                     "cot", new CCUnaryOperator((n) -> Math.cos(n) / Math.sin(n), 0, null));
+        final Map<String, CCBinaryOperator> binaryOpMap = Map.of("log", new CCBinaryOperator((n, b) -> Math.log(n) / Math.log(b), 4, null),
+                     "nthRoot", new CCBinaryOperator((n, i) -> Math.pow(n, 1 / i), 6, Type.RIGHT),
+                     "potenza", new CCBinaryOperator((n, e) -> Math.pow(n, e), 5, Type.RIGHT));
+        final Map<String, CCUnaryOperator> unaryOpMap = Map.of("ln", new CCUnaryOperator((n) -> Math.log(n), 1, null),
+                     "abs", new CCUnaryOperator((n) -> Math.abs(n), 1, null),
+                     "factorial", new CCUnaryOperator((n) -> (double) LongStream.rangeClosed(1, Math.round(n)).reduce(1, (long n1, long n2) -> n1 * n2), 1, null),
+                     "sin", new CCUnaryOperator((n) -> Math.sin(n), 1, null),
+                     "cos", new CCUnaryOperator((n) -> Math.cos(n), 1, null),
+                     "tan", new CCUnaryOperator((n) -> Math.tan(n), 1, null),
+                     "csc", new CCUnaryOperator((n) -> 1 / Math.sin(n), 1, null),
+                     "sec", new CCUnaryOperator((n) -> 1 / Math.cos(n), 1, null),
+                     "cot", new CCUnaryOperator((n) -> Math.cos(n) / Math.sin(n), 1, null));
         binaryOpMap.putAll(StandardCalculatorModelFactory.create().getBinaryOpMap());
         unaryOpMap.putAll(StandardCalculatorModelFactory.create().getUnaryOpMap());
 
