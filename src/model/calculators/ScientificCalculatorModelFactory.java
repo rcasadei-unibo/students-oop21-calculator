bd4018e9034fa810d78af6f946cb6315c6db5153
Riccardo Tassinari <riccardo.tassinari9@studio.unibo.it>
2022-04-04 13:19:11 +0200

    adapted the ScientificCalculatorModel class to the recent changes

--- src/model/calculators/ScientificCalculatorModelFactory.java
+++ src/model/calculators/ScientificCalculatorModelFactory.java
@@ -11,21 +11,22 @@ import utils.CCUnaryOperator;
  *
  */
 
-public class ScientificCalculatorModel extends CalculatorModelTemplate {
+public final class ScientificCalculatorModelFactory {
     /**
      * ScientificCalculatorModel contains all the operations of StandardCalculatorModel
      * and new operations unique to a scientific calculator.
      *
      */
-    private StandardCalculatorModel standard = new StandardCalculatorModel();
+    private ScientificCalculatorModelFactory() {
+    }
     /**
-     * 
+     * @return CalculatorModelTemplate
      */
-    public ScientificCalculatorModel() {
-        super(Map.of("log", new CCBinaryOperator((n, b) -> Math.log(n) / Math.log(b), 0, null),
+    public static CalculatorModel create() {
+        final Map<String, CCBinaryOperator> binaryOpMap = Map.of("log", new CCBinaryOperator((n, b) -> Math.log(n) / Math.log(b), 0, null),
                      "nthRoot", new CCBinaryOperator((n, i) -> Math.pow(n, 1 / i), 0, null),
-                     "potenza", new CCBinaryOperator((n, e) -> Math.pow(n, e), 0, null)),
-              Map.of("ln", new CCUnaryOperator((n) -> Math.log(n), 0, null),
+                     "potenza", new CCBinaryOperator((n, e) -> Math.pow(n, e), 0, null));
+        final Map<String, CCUnaryOperator> unaryOpMap = Map.of("ln", new CCUnaryOperator((n) -> Math.log(n), 0, null),
                      "abs", new CCUnaryOperator((n) -> Math.abs(n), 0, null),
                      "factorial", new CCUnaryOperator((n) -> (double) LongStream.rangeClosed(1, Math.round(n)).reduce(1, (long n1, long n2) -> n1 * n2), 0, null),
                      "sin", new CCUnaryOperator((n) -> Math.sin(n), 0, null),
@@ -33,8 +34,8 @@ public class ScientificCalculatorModel extends CalculatorModelTemplate {
                      "tan", new CCUnaryOperator((n) -> Math.tan(n), 0, null),
                      "csc", new CCUnaryOperator((n) -> 1 / Math.sin(n), 0, null),
                      "sec", new CCUnaryOperator((n) -> 1 / Math.cos(n), 0, null),
-                     "cot", new CCUnaryOperator((n) -> Math.cos(n) / Math.sin(n), 0, null)));
+                     "cot", new CCUnaryOperator((n) -> Math.cos(n) / Math.sin(n), 0, null));
 
-        this.addOperations(standard.getUnaryOpMap(), standard.getBinaryOpMap());
+        return new CalculatorModelTemplate(binaryOpMap, unaryOpMap);
     }
 }
