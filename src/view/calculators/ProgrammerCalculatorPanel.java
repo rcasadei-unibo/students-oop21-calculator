92bb43f53e815d4cadda739287792e3e90be9f75
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-04-05 12:22:47 +0200

    added functionality to conversion buttons
    now buttons disable numbers that are not used in the conversion base

--- src/view/calculators/ProgrammerCalculatorPanel.java
+++ src/view/calculators/ProgrammerCalculatorPanel.java
@@ -11,8 +11,10 @@ import java.util.Map;
 
 import javax.swing.JButton;
 import javax.swing.JPanel;
+
 import controller.calculators.CalculatorController;
 import model.calculators.ProgrammerCalculatorModelFactory;
+import utils.ConversionAlgorithms;
 import view.components.CCDisplay;
 import view.components.CCNumPad;
 
@@ -29,8 +31,7 @@ public class ProgrammerCalculatorPanel extends JPanel {
     private static final long serialVersionUID = -8342823219976507443L;
     private final CalculatorController controller;
     private final CCDisplay display = new CCDisplay();
-    private final Map<String, CCDisplay> convDisplays = new HashMap<>();
-    private final List<JButton> hexadecimalLetters = new ArrayList<>();
+    private HexadecimalLettersPanel hexaLetters;
     private final CCNumPad numpad;
     private ActionListener opAl;
     {
@@ -58,7 +59,7 @@ public class ProgrammerCalculatorPanel extends JPanel {
         };
         this.numpad = new CCNumPad(btnAl, calcAl, backspaceAl);
     }
-    
+
     /**
      * @param controller
      */
@@ -66,54 +67,48 @@ public class ProgrammerCalculatorPanel extends JPanel {
         this.controller = controller;
         this.setPanels();
     }
+
     private void setPanels() {
         this.setLayout(new BorderLayout());
         this.add(this.display, BorderLayout.NORTH);
         this.setConversionPanel();
         this.setNumpad();
     }
+
     private void setConversionPanel() {
         final ActionListener conv = new ActionListener() {
 
             @Override
             public void actionPerformed(final ActionEvent e) {
-                //TODO fix
                 final JButton btn = (JButton) e.getSource();
-                convDisplays.get(btn.getText()).updateText(btn.getText());
+                switch (btn.getText()) {
+                case "HEX":
+                    hexaLetters.enableAll();
+                    enableButtons(10);
+                    break;
+                case "DEC":
+                    hexaLetters.disableAll();
+                    enableButtons(10);
+                    break;
+                case "OCT":
+                    hexaLetters.disableAll();
+                    enableButtons(8);
+                    break;
+                case "BIN":
+                    hexaLetters.disableAll();
+                    enableButtons(2);
+                    break;
+                default:
+                    break;
+                }
             }
         };
-        final JPanel conversionButtons = new JPanel();
-        conversionButtons.setLayout(new GridLayout(4, 2));
-        final JButton hex = new JButton("HEX");
-        hex.addActionListener(conv);
-        conversionButtons.add(hex);
-        final CCDisplay hexDisplay = new CCDisplay();
-        conversionButtons.add(hexDisplay);
-        convDisplays.put("HEX",hexDisplay);
-        final JButton dec = new JButton("DEC");
-        dec.addActionListener(conv);
-        conversionButtons.add(dec);
-        final CCDisplay decDisplay = new CCDisplay();
-        conversionButtons.add(decDisplay);
-        convDisplays.put("HEX",decDisplay);
-        final JButton oct = new JButton("OCT");
-        oct.addActionListener(conv);
-        conversionButtons.add(oct);
-        final CCDisplay octDisplay = new CCDisplay();
-        conversionButtons.add(octDisplay);
-        convDisplays.put("HEX",octDisplay);
-        final JButton bin = new JButton("BIN");
-        bin.addActionListener(conv);
-        conversionButtons.add(bin);
-        final CCDisplay binDisplay = new CCDisplay();
-        conversionButtons.add(binDisplay);
-        convDisplays.put("HEX",binDisplay);
-        
-        this.add(conversionButtons,BorderLayout.CENTER);
+        this.add(new ConversionPanel(conv), BorderLayout.CENTER);
     }
+
     private JPanel getBitwiseOperators() {
         final JPanel operators = new JPanel();
-        
+
         for (final var entry : ProgrammerCalculatorModelFactory.create().getBinaryOpMap().entrySet()) {
             if (this.equalsAny(entry.getKey())) {
                 final JButton btn = new JButton(entry.getKey());
@@ -121,13 +116,35 @@ public class ProgrammerCalculatorPanel extends JPanel {
                 btn.add(operators);
             }
         }
-        
-        
-        
+
         return operators;
-        
+
     }
-    
+    private void enableButtons(final int i) {
+        final var numbers = this.getNumbers();
+        numbers.entrySet().stream().filter((entry) -> 
+            Integer.parseInt(entry.getKey()) < i).forEach((entry) -> entry.getValue().setEnabled(true));
+        numbers.entrySet().stream().filter((entry) -> 
+            Integer.parseInt(entry.getKey()) >= i).forEach((entry) -> entry.getValue().setEnabled(false));
+    }
+    private Map<String, JButton> getNumbers() {
+        final Map<String, JButton> map = new HashMap<>();
+        this.numpad.getButtons().entrySet().stream().filter((entry) -> { 
+            try {
+                Integer.parseInt(entry.getKey());
+                return true;
+            } catch (final NumberFormatException e) {
+                return false;
+            }
+        }).forEach((entry) -> map.put(entry.getKey(), entry.getValue()));
+        return map;
+    }
+    /**
+     * 
+     * @param key
+     * @return true if key == 
+     * ror , rol , shiftL, shiftR, nand
+     */
     private boolean equalsAny(final String key) {
         switch (key) {
         case "ror":
@@ -144,50 +161,47 @@ public class ProgrammerCalculatorPanel extends JPanel {
             return false;
         }
     }
+
     private void setNumpad() {
-        
+
         final JPanel numpad = new JPanel();
-        numpad.setLayout(new GridLayout(1, 3)); //TODO USELESS
-        
-        numpad.add(this.getHexadecimalLettersPanel());
-        
-        
+        numpad.setLayout(new GridLayout(1, 3));
+        // TODO add ActionListener for hexadecimal letters
+        this.hexaLetters = new HexadecimalLettersPanel(null);
+        numpad.add(this.hexaLetters);
         numpad.add(this.getMiddleNumpad());
-        
         numpad.add(this.getRightNumpad());
-        
-        
-        
-        
+
         final JPanel numpadAndOperators = new JPanel();
         numpadAndOperators.setLayout(new BorderLayout());
-        numpadAndOperators.add(this.getBitwiseOperators(),BorderLayout.NORTH);
-        numpadAndOperators.add(numpad,BorderLayout.CENTER);
-        
-        this.add(numpadAndOperators,BorderLayout.SOUTH);
-        
+        numpadAndOperators.add(this.getBitwiseOperators(), BorderLayout.NORTH);
+        numpadAndOperators.add(numpad, BorderLayout.CENTER);
+
+        this.add(numpadAndOperators, BorderLayout.SOUTH);
+
     }
+
     private JPanel getRightNumpad() {
         final JPanel operators = new JPanel();
         operators.setLayout(new GridLayout(6, 1));
-        
+
         final JButton not = new JButton("not");
         not.addActionListener(opAl);
         final JButton nor = new JButton("nor");
         nor.addActionListener(opAl);
-        
+
         operators.add(not);
         operators.add(nor);
-        
-        for ( final var entry : ProgrammerCalculatorModelFactory.getBasicOperators().entrySet()) {
-                final JButton btn = new JButton(entry.getKey());
-                btn.addActionListener(opAl);
-                operators.add(btn);
+
+        for (final var entry : ProgrammerCalculatorModelFactory.getBasicOperators().entrySet()) {
+            final JButton btn = new JButton(entry.getKey());
+            btn.addActionListener(opAl);
+            operators.add(btn);
         }
-        
-        
+
         return operators;
     }
+
     private JPanel getMiddleNumpad() {
         final JPanel panel = new JPanel();
         panel.setLayout(new BorderLayout());
@@ -203,43 +217,135 @@ public class ProgrammerCalculatorPanel extends JPanel {
         topMiddleNumpad.add(and);
         topMiddleNumpad.add(or);
         topMiddleNumpad.add(xor);
-        panel.add(topMiddleNumpad,BorderLayout.NORTH);
+        panel.add(topMiddleNumpad, BorderLayout.NORTH);
         return panel;
     }
-    private JPanel getHexadecimalLettersPanel() {
-        final ActionListener al = new ActionListener() {
-            @Override
-            public void actionPerformed(final ActionEvent e) {
-                // TODO Auto-generated method stub
-            }
-        };
-        final JPanel panel = new JPanel();
-        panel.setLayout(new GridLayout(6, 1));
-        final JButton a = new JButton("A");
-        a.addActionListener(al);
-        this.hexadecimalLetters.add(a);
-        panel.add(a);
-        final JButton b = new JButton("B");
-        b.addActionListener(al);
-        this.hexadecimalLetters.add(b);
-        panel.add(b);
-        final JButton c = new JButton("C");
-        c.addActionListener(al);
-        this.hexadecimalLetters.add(c);
-        panel.add(c);
-        final JButton d = new JButton("D");
-        d.addActionListener(al);
-        this.hexadecimalLetters.add(d);
-        panel.add(d);
-        final JButton e = new JButton("E");
-        e.addActionListener(al);
-        this.hexadecimalLetters.add(e);
-        panel.add(e);
-        final JButton f = new JButton("F");
-        f.addActionListener(al);
-        this.hexadecimalLetters.add(f);
-        panel.add(f);
+    
+    private class ConversionPanel extends JPanel{
+        /**
+         * 
+         */
+        private static final long serialVersionUID = -9080067811293897721L;
         
-        return panel;
+        private final Map<String, CCDisplay> map = new HashMap<>();
+        
+        private final CCDisplay decDisplay;
+        ConversionPanel(final ActionListener conv) {
+          //TODO fix
+            this.setLayout(new GridLayout(4, 2));
+            
+            
+            final JButton hex = new JButton("HEX");
+            hex.addActionListener(conv);
+            this.add(hex);
+            final CCDisplay hexDisplay = new CCDisplay();
+            this.add(hexDisplay);
+            this.map.put(hex.getText(), hexDisplay);
+            
+            final JButton dec = new JButton("DEC");
+            dec.addActionListener(conv);
+            this.add(dec);
+            this.decDisplay = new CCDisplay();
+            this.add(decDisplay);
+            //this.map.put(dec.getText(), decDisplay);
+            //not needed since it's linked to the main display
+            
+            final JButton oct = new JButton("OCT");
+            oct.addActionListener(conv);
+            this.add(oct);
+            final CCDisplay octDisplay = new CCDisplay();
+            this.add(octDisplay);
+            this.map.put(oct.getText(), octDisplay);
+            
+            final JButton bin = new JButton("BIN");
+            bin.addActionListener(conv);
+            this.add(bin);
+            final CCDisplay binDisplay = new CCDisplay();
+            this.add(binDisplay);
+            this.map.put(bin.getText(), binDisplay);
+        }
+        /**
+         * @param value for base2 base8 and base16.
+         */
+        void updateConvDisplays(int value) {
+            for (final var entry : this.map.entrySet()) {
+                entry.getValue().updateText(ConversionAlgorithms.conversionToStringBase(textToBase(entry.getKey()), value));
+            }
+        }
+        /**
+         * 
+         * @param text string to show for base10 display.
+         */
+        void updateDecDisplay(String text) {
+            this.decDisplay.updateText(text);
+        }
+        private int textToBase(final String text) {
+            switch(text) {
+            case "HEX":
+                return 16;
+            case "DEC":
+                return 10;
+            case "OCT":
+                return 8;
+            case "BIN":
+                return 2;
+            default:
+                return 10;
+            }
+        }
     }
+    private class HexadecimalLettersPanel extends JPanel {
+        /**
+         * 
+         */
+        private static final long serialVersionUID = -2613278018688810576L;
+
+        private final List<JButton> hexadecimalLetters = new ArrayList<>();
+        private final ActionListener al;
+        /**
+         * 
+         * @param al
+         */
+        HexadecimalLettersPanel(final ActionListener al) {
+            this.al = al;
+            this.setLayout(new GridLayout(6, 1));
+            final JButton a = new JButton("A");
+            a.addActionListener(al);
+            this.hexadecimalLetters.add(a);
+            this.add(a);
+            final JButton b = new JButton("B");
+            b.addActionListener(al);
+            this.hexadecimalLetters.add(b);
+            this.add(b);
+            final JButton c = new JButton("C");
+            c.addActionListener(al);
+            this.hexadecimalLetters.add(c);
+            this.add(c);
+            final JButton d = new JButton("D");
+            d.addActionListener(al);
+            this.hexadecimalLetters.add(d);
+            this.add(d);
+            final JButton e = new JButton("E");
+            e.addActionListener(al);
+            this.hexadecimalLetters.add(e);
+            this.add(e);
+            final JButton f = new JButton("F");
+            f.addActionListener(al);
+            this.hexadecimalLetters.add(f);
+            this.add(f);
+        }
+        /**
+         * This method disables all hexadecimal letter buttons.
+         */
+        void disableAll() {
+            this.hexadecimalLetters.forEach((btn) -> btn.setEnabled(false));
+        }
+        /**
+         * This method enables all hexadecimal letter buttons.
+         */
+        void enableAll() {
+            this.hexadecimalLetters.forEach((btn) -> btn.setEnabled(true));
+        }
+    }
+
 }
