be9859b2509d15d8c0db93845f726b7aec2aa2c9
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-04-17 12:19:14 +0200

    added javadoc

--- src/view/calculators/StandardCalculatorPanel.java
+++ src/view/calculators/StandardCalculatorPanel.java
@@ -3,23 +3,24 @@ import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JButton;
 import javax.swing.JPanel;
-
 import view.components.CCDisplay;
 import view.components.CCNumPad;
-
 import controller.calculators.CalculatorController;
 import model.calculators.StandardCalculatorModelFactory;
+import model.manager.EngineModelInterface.Calculator;
 import utils.CreateButton;
-//TODO MISSING JAVADOC.
 /**
- * 
- * MISSING JAVADOC.
- *
+ * This is StandardCalculatorPanel which holds the basic operators:
+ * -plus.
+ * -subtraction.
+ * -multiplication.
+ * -division.
+ * -square.
+ * -inverse.
+ * -modulo.
  */
-
 public class StandardCalculatorPanel extends JPanel {
 
     /**
@@ -28,14 +29,18 @@ public class StandardCalculatorPanel extends JPanel {
     private static final long serialVersionUID = -3801351406960094788L;
     private final CCDisplay display = new CCDisplay();
     private final CalculatorController controller;
-    //TODO MISSING JAVADOC.
     /**
-     * MISSING JAVADOC.
-     * @param controller 
-     * 
+      * This is StandardCalculatorPanel which holds the basic operators:
+      * -plus.
+      * -subtraction.
+      * -multiplication.
+      * -division.
+      * -square.
+      * -inverse.
+      * -modulo.
      */
-    public StandardCalculatorPanel(final CalculatorController controller) {
-        this.controller = controller;
+    public StandardCalculatorPanel() {
+        this.controller = Calculator.STANDARD.getController();
         this.setLayout(new BorderLayout());
         this.add(display, BorderLayout.NORTH);
 
@@ -55,7 +60,9 @@ public class StandardCalculatorPanel extends JPanel {
             @Override
             public void actionPerformed(final ActionEvent e) {
                 if (!controller.getManager().memory().getCurrentState().isEmpty() && !(controller.getManager().memory().getCurrentState().contains("Syntax error") || controller.getManager().memory().getCurrentState().contains("Syntax Error"))) {
+                    final String history = controller.getManager().memory().getCurrentState().stream().reduce("", (a, b) -> a + b);
                     controller.getManager().engine().calculate();
+                    controller.getManager().memory().addResult(history.concat(" = ").concat(controller.getManager().memory().getCurrentState().stream().reduce("", (a, b) -> a + b)));
                 }
                 CreateButton.updateDisplay(controller, display);
             }
@@ -85,18 +92,9 @@ public class StandardCalculatorPanel extends JPanel {
         this.add(operator, BorderLayout.EAST);
     }
     /**
-     * 
-     * @return controller
+     * Returns the StandardCalculatorPanel's display.
+     * @return display 
      */
-
-    public CalculatorController getController() {
-        return this.controller;
-    }
-    /**
-     * 
-     * @return display
-     */
-
     public CCDisplay getDisplay() {
         return this.display;
     }
