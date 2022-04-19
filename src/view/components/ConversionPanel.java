49b638c38778be8e1a804857536783823ddd11ce
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-04-19 17:39:40 +0200

    new color change and create button method

--- src/view/components/ConversionPanel.java
+++ src/view/components/ConversionPanel.java
@@ -18,7 +18,8 @@ public class ConversionPanel extends JPanel {
      */
     private static final long serialVersionUID = -9080067811293897721L;
 
-    private final Map<String, CCDisplay> map = new HashMap<>();
+    private final Map<String, CCDisplay> displayMap = new HashMap<>();
+    private final Map<String, JButton> buttonsMap = new HashMap<>();
     /**
      * This class contains 4 Buttons and 4 Displays that are linked.
      * Each display shows its buttons conversion base.
@@ -27,47 +28,30 @@ public class ConversionPanel extends JPanel {
      */
     public ConversionPanel(final ActionListener conv) {
         this.setLayout(new GridLayout(4, 2));
-
-        final JButton hex = new JButton("HEX");
-        hex.addActionListener(conv);
-        hex.setBackground(CCColors.CONVERSION_BUTTON);
-        this.add(hex);
-        final CCDisplay hexDisplay = new CCDisplay();
-        this.add(hexDisplay);
-        this.map.put(hex.getText(), hexDisplay);
-
-        final JButton dec = new JButton("DEC");
-        dec.addActionListener(conv);
-        dec.setBackground(CCColors.CONVERSION_BUTTON);
-        this.add(dec);
-        final CCDisplay decDisplay = new CCDisplay();
-        this.add(decDisplay);
-        this.map.put(dec.getText(), decDisplay);
-
-        final JButton oct = new JButton("OCT");
-        oct.addActionListener(conv);
-        oct.setBackground(CCColors.CONVERSION_BUTTON);
-        this.add(oct);
-        final CCDisplay octDisplay = new CCDisplay();
-        this.add(octDisplay);
-        this.map.put(oct.getText(), octDisplay);
-
-        final JButton bin = new JButton("BIN");
-        bin.addActionListener(conv);
-        bin.setBackground(CCColors.CONVERSION_BUTTON);
-        this.add(bin);
-        final CCDisplay binDisplay = new CCDisplay();
-        this.add(binDisplay);
-        this.map.put(bin.getText(), binDisplay);
-
+        this.createButton("HEX", conv);
+        this.createButton("DEC", conv);
+        this.createButton("OCT", conv);
+        this.createButton("BIN", conv);
     }
     /**
      * @param l the number that the displays will show
      */
     public void updateConvDisplays(final long l) {
-        this.map.entrySet().stream().forEach((entry) -> entry.getValue().updateText(textToBase(entry.getKey(), l)));
+        this.displayMap.entrySet().stream().forEach((entry) -> entry.getValue().updateText(textToBase(entry.getKey(), l)));
     }
-
+    /**
+     * Updated the buttons' color to indicate it is active.
+     * @param text the button's text.
+     */
+    public void changeToActive(final String text) {
+        this.buttonsMap.forEach((str, btn) -> {
+            if (text.equals(str)) {
+                btn.setBackground(CCColors.ACTIVE_CONVERSION_BUTTON);
+            } else {
+                btn.setBackground(CCColors.CONVERSION_BUTTON);
+            }
+        });
+    }
     private String textToBase(final String text, final long l) {
         switch (text) {
         case "HEX":
@@ -81,5 +65,15 @@ public class ConversionPanel extends JPanel {
         default:
             return null;
         }
+    }
+    private void createButton(final String text, final ActionListener conv) {
+        final JButton btn = new JButton(text);
+        btn.addActionListener(conv);
+        btn.setBackground(CCColors.CONVERSION_BUTTON);
+        this.add(btn);
+        final CCDisplay display = new CCDisplay();
+        this.add(display);
+        this.displayMap.put(btn.getText(), display);
+        this.buttonsMap.put(text, btn);
     }
 }
