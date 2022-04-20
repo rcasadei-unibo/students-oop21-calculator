db881a57b43a6647d7b331ed623a85dde47c7f51
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-04-20 17:00:52 +0200

    untested outputformatter

--- src/view/logics/ScientificOutputFormatter.java
+++ src/view/logics/ScientificOutputFormatter.java
@@ -1,29 +1,75 @@
 package view.logics;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import controller.calculators.CalculatorController;
+import model.calculators.ScientificCalculatorModelFactory;
+import model.manager.EngineModelInterface.Calculator;
 import view.components.CCDisplay;
 
-public class ScientificOutputFormatter implements OutputFormatterLogics {
-
-    public ScientificOutputFormatter(CCDisplay display) {
-        // TODO Auto-generated constructor stub
+public class ScientificOutputFormatter extends StandardOutputFormatter{
+    private final CalculatorController controller = Calculator.SCIENTIFIC.getController();
+    private final CCDisplay display;
+    {
+        this.setAppearanceMap();
     }
-
+    /**
+     * 
+     * @param display
+     */
+    public ScientificOutputFormatter(CCDisplay display) {
+        super(display);
+        this.display = display;
+    }
+    /**
+     * This will update the display using the format() method's output.
+     */
     @Override
     public void updateDisplay() {
-        // TODO Auto-generated method stub
-
+        this.display.updateText(this.format());
     }
-
-    @Override
-    public String format() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
     @Override
     public void updateDisplayUpperText() {
-        // TODO Auto-generated method stub
-
+        this.display.updateUpperText(this.format().concat(" ="));
+    }
+    @Override
+    public String format() {
+        final List<String> output = this.replaceOp();
+        return this.getString(output);
+    }
+    private List<String> replaceOp() {
+        final List<String> state = new ArrayList<>(controller.getManager().memory().getCurrentState());
+        return state.stream().map((str) -> {
+                if (controller.isBinaryOperator(str) || controller.isUnaryOperator(str)) {
+                    return appearanceMap.get(str);
+                }
+                return str;
+        }).collect(Collectors.toList());
+    }
+    private String getString(final List<String> input) {
+        return input.stream().reduce("", (a, b) -> a + b);
+    }
+    private void setAppearanceMap() {
+        ScientificCalculatorModelFactory.create().getUnaryOpMap().forEach((str, op) -> {
+            switch (str) {
+                case "factorial":
+                    appearanceMap.put(str, "!");
+                    break;
+                default:
+                    break;
+            }
+        });
+        ScientificCalculatorModelFactory.create().getBinaryOpMap().forEach((str, op) -> {
+            switch (str) {
+                case "test":
+                    break;
+                default:
+                    appearanceMap.put(str, str);
+                    break;
+            }
+        });
     }
 
 }
