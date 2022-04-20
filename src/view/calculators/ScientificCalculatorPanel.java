d9c505bd448dff96b36c283997906e0d1452a64d
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-04-20 16:47:52 +0200

    changed to use new formatters

--- src/view/calculators/ScientificCalculatorPanel.java
+++ src/view/calculators/ScientificCalculatorPanel.java
@@ -2,47 +2,105 @@ package view.calculators;
 
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JButton;
 import javax.swing.JPanel;
 
-import controller.calculators.CalculatorController;
+import model.calculators.ScientificCalculatorModelFactory;
+import model.calculators.StandardCalculatorModelFactory;
 import model.manager.EngineModelInterface.Calculator;
+import view.components.CCDisplay;
+import view.components.CCNumPad;
 import view.logics.CreateButton;
+import view.logics.ScientificInputFormatter;
+import view.logics.ScientificOutputFormatter;
 /**
  * 
  * MISSING JAVADOC.
  *
  */
 
-public class ScientificCalculatorPanel extends StandardCalculatorPanel {
+public class ScientificCalculatorPanel extends JPanel{
     /**
      * 
      */
     private static final long serialVersionUID = 1795172134074823312L;
-    private static final CalculatorController CONTROLLER = Calculator.SCIENTIFIC.getController();
+    
+    private final ScientificInputFormatter inFormatter = new ScientificInputFormatter();
+    private final CCDisplay display = new CCDisplay();
+    private final ScientificOutputFormatter outFormatter = new ScientificOutputFormatter(this.display);
+    private final CCNumPad numpad;
+    private final transient ActionListener opAl;
+    {
+        opAl = new ActionListener() {
+            
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                inFormatter.read(((JButton)e.getSource()).getText());
+                outFormatter.updateDisplay();
+            }
+        };
+        
+        this.numpad = new CCNumPad(
+        opAl, calcAl -> {
+            final String history = outFormatter.format();
+            outFormatter.updateDisplayUpperText();
+            inFormatter.calculate();
+            outFormatter.updateDisplay();
+            Calculator.SCIENTIFIC.getController().getManager().memory().addResult(history.concat("=").concat(history).concat(
+                    Calculator.SCIENTIFIC.getController().getManager().memory().getCurrentState().stream().reduce("", (a,b) -> a + b)));
+        }, deleteAl -> {
+            inFormatter.deleteLast();
+            outFormatter.updateDisplay();
+        });
+    }
     /**
      * 
      */
     public ScientificCalculatorPanel() {
-        super();
+        this.setLayout(new BorderLayout());
+        this.add(this.getStandardPanel(), BorderLayout.CENTER);
         this.add(this.getScientificOperators(), BorderLayout.WEST);
+    }
+
+    private JPanel getStandardPanel() {
+        final JPanel standard = new JPanel();
+        standard.setLayout(new BorderLayout());
+        
+        standard.add(this.numpad, BorderLayout.CENTER);
+        final JPanel standardOp = new JPanel();
+        standardOp.setLayout(new GridLayout(4, 2));
+        StandardCalculatorModelFactory.create().getBinaryOpMap().entrySet().forEach((entry) -> {
+            standardOp.add(this.setActionListener(entry.getKey()));
+        });
+        StandardCalculatorModelFactory.create().getUnaryOpMap().entrySet().forEach((entry) -> {
+            standardOp.add(this.setActionListener(entry.getKey()));
+        });
+        
+        
+        return standard;
     }
 
     private JPanel getScientificOperators() {
        final JPanel scientificOperators = new JPanel();
        scientificOperators.setLayout(new GridLayout(4, 3));
-       scientificOperators.add(CreateButton.createOpButton("^", "^", "^", CONTROLLER, getDisplay()));
-       scientificOperators.add(CreateButton.createOpButton("nthRoot", "nthRoot", "nthRoot", CONTROLLER, getDisplay()));
-       scientificOperators.add(CreateButton.createOpButton("ln", "ln", "ln", CONTROLLER, getDisplay()));
-       scientificOperators.add(CreateButton.createOpButton("log", "log", "log", CONTROLLER, getDisplay()));
-       scientificOperators.add(CreateButton.createOpButton("Abs", "abs", "abs", CONTROLLER, getDisplay()));
-       scientificOperators.add(CreateButton.createOpButton("!", "factorial", "!", CONTROLLER, getDisplay()));
-       scientificOperators.add(CreateButton.createOpButton("sin", "sin", "sin", CONTROLLER, getDisplay()));
-       scientificOperators.add(CreateButton.createOpButton("cos", "cos", "cos", CONTROLLER, getDisplay()));
-       scientificOperators.add(CreateButton.createOpButton("tan", "tan", "tan", CONTROLLER, getDisplay()));
-       scientificOperators.add(CreateButton.createOpButton("csc", "csc", "csc", CONTROLLER, getDisplay()));
-       scientificOperators.add(CreateButton.createOpButton("sec", "sec", "sec", CONTROLLER, getDisplay()));
-       scientificOperators.add(CreateButton.createOpButton("cot", "cot", "cot", CONTROLLER, getDisplay()));
+       ScientificCalculatorModelFactory.create().getBinaryOpMap().entrySet().forEach((entry) -> {
+           scientificOperators.add(this.setActionListener(entry.getKey()));
+       });
+       ScientificCalculatorModelFactory.create().getUnaryOpMap().entrySet().forEach((entry) -> {
+           scientificOperators.add(this.setActionListener(entry.getKey()));
+       });
        return scientificOperators;
+    }
+    private JButton setActionListener(final String text) {
+        final var btn = CreateButton.createOpButtonFR(text);
+        btn.addActionListener(e -> {
+            this.inFormatter.read(((JButton)e.getSource()).getText());
+            this.outFormatter.updateDisplay();
+        });
+        return btn;
     }
 
 }
