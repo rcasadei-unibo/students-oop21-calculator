b4cb5e24717f9b997f3401bf0309a501080458cf
Alessandro Agosta <alessandro.agosta@studio.unibo.it>
2022-04-20 16:47:39 +0200

    skeleton of Scientific Input and Output formatters

---
+++ src/view/logics/ScientificInputFormatter.java
@@ -0,0 +1,23 @@
+package view.logics;
+
+public class ScientificInputFormatter implements InputFormatterLogics {
+
+    @Override
+    public void read(String input) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void deleteLast() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public void calculate() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
---
+++ src/view/logics/ScientificOutputFormatter.java
@@ -0,0 +1,29 @@
+package view.logics;
+
+import view.components.CCDisplay;
+
+public class ScientificOutputFormatter implements OutputFormatterLogics {
+
+    public ScientificOutputFormatter(CCDisplay display) {
+        // TODO Auto-generated constructor stub
+    }
+
+    @Override
+    public void updateDisplay() {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public String format() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void updateDisplayUpperText() {
+        // TODO Auto-generated method stub
+
+    }
+
+}
