caba11ec87cdacb82e2cd4fe5c05ee8005a5fe96
Riccardo Tassinari <riccardo.tassinari9@studio.unibo.it>
2022-04-24 13:30:20 +0200

    added some tests

--- src/test/ScientificCalculatorOperationsTest.java
+++ src/test/ScientificCalculatorOperationsTest.java
@@ -1,10 +1,8 @@
 package test;
 
 import static org.junit.Assert.assertEquals;
-
 import model.calculators.CalculatorModel;
 import model.calculators.ScientificCalculatorModelFactory;
-
 /**
  * 
  * 
@@ -20,10 +18,10 @@ public class ScientificCalculatorOperationsTest {
     @org.junit.Test
     public void logarithmTest() {
         final var ln = this.calc.getUnaryOpMap().get("ln");
-        final var log = this.calc.getBinaryOpMap().get("log");
-        assertEquals(1.0, log.apply(10.0, 10.0), 0);
-        assertEquals(0.0, log.apply(1.0, 2.0), 0);
-        assertEquals(3.0, log.apply(8.0, 2.0), 0);
+        final var log = this.calc.getUnaryOpMap().get("log");
+        assertEquals(1.0, log.apply(10.0), 0);
+        assertEquals(0.0, log.apply(1.0), 0);
+        assertEquals(2.0, log.apply(100), 0);
         assertEquals(1.0, ln.apply(Math.exp(1.0)), 0);
     }
     /**
@@ -58,9 +56,9 @@ public class ScientificCalculatorOperationsTest {
      */
     @org.junit.Test
     public void rootTest() {
-        final var root = this.calc.getBinaryOpMap().get("nthRoot");
-        assertEquals(9.0, root.apply(81, 2), 0);
-        assertEquals(1.0, root.apply(1, 3), 0);
+        final var root = this.calc.getBinaryOpMap().get("root");
+        assertEquals(9.0, root.apply(2, 81), 0);
+        assertEquals(1.0, root.apply(3, 1), 0);
     }
     /**
      * Method that tests the "^" operator.
@@ -69,31 +67,68 @@ public class ScientificCalculatorOperationsTest {
     public void potenzaTest() {
         final var pot = this.calc.getBinaryOpMap().get("^");
         assertEquals(25.0, pot.apply(5.0, 2.0), 0);
-        assertEquals(10000.0, pot.apply(10.0, 4), 0);
+        assertEquals(10_000.0, pot.apply(10.0, 4), 0);
         assertEquals(-1.0, pot.apply(-1, 3), 0);
         assertEquals(1.0, pot.apply(-1.0, 20), 0);
     }
     /**
-     * Method that tests the sin operator "sin".
+     * Method that tests the operator "sin".
      */
     @org.junit.Test
     public void sinTest() {
         final var sin = this.calc.getUnaryOpMap().get("sin");
         assertEquals(0, sin.apply(0), 0);
+        assertEquals(0.841_4, sin.apply(1), 0.0001);
+        assertEquals(-0.841_4, sin.apply(-1), 0.0001);
     }
     /**
-     * Method that tests the cos operator "cos".
+     * Method that tests the operator "cos".
      */
     @org.junit.Test
     public void cosTest() {
-
+        final var cos = this.calc.getUnaryOpMap().get("cos");
+        assertEquals(1, cos.apply(0), 0);
+        assertEquals(0.540_3, cos.apply(1), 0.0001);
+        assertEquals(0.540_3, cos.apply(-1), 0.0001);
     }
     /**
-     * Method that tests the tan operator "tan".
+     * Method that tests the operator "tan".
      */
     @org.junit.Test
     public void tanTest() {
-
+        final var tan = this.calc.getUnaryOpMap().get("tan");
+        assertEquals(0, tan.apply(0), 0);
+        assertEquals(1.557_4, tan.apply(1), 0.0001);
+        assertEquals(-1.557_4, tan.apply(-1), 0.0001);
     }
-
+    /**
+     * Method that tests the operator "csc".
+     */
+    @org.junit.Test
+    public void cscTest() {
+        final var csc = this.calc.getUnaryOpMap().get("csc");
+        assertEquals(1.188_3, csc.apply(1), 0.0001);
+        assertEquals(-1.188_3, csc.apply(-1), 0.0001);
+        assertEquals(100, csc.apply(0.01), 1);
+    }
+    /**
+     * Method that tests the operator "sec".
+     */
+    @org.junit.Test
+    public void secTest() {
+        final var sec = this.calc.getUnaryOpMap().get("sec");
+        assertEquals(1.850_8, sec.apply(1), 0.0001);
+        assertEquals(1.850_8, sec.apply(-1), 0.0001);
+        assertEquals(1.00, sec.apply(0.01), 0.001);
+    }
+    /**
+     * Method that tests the operator "cot".
+     */
+    @org.junit.Test
+    public void cotTest() {
+        final var cot = this.calc.getUnaryOpMap().get("cot");
+        assertEquals(0.642_0, cot.apply(1), 0.0001);
+        assertEquals(-0.6420, cot.apply(-1), 0.0001);
+        assertEquals(99.996, cot.apply(0.01), 0.001);
+    }
 }
